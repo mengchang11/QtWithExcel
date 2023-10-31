ea1dd897717db41190b63038b6cf4018c0c88acb
mengc <3431974037@qq.com>
2023-10-31 21:12:19 +0800

    工作表显示

--- ExcelDemo_M/exceldemo_m.cpp
+++ ExcelDemo_M/exceldemo_m.cpp
@@ -1,6 +1,8 @@
 #include "exceldemo_m.h"
 #include "ui_exceldemo_m.h"
 
+#include <QDebug>
+
 ExcelDemo_M::ExcelDemo_M(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::ExcelDemo_M)
@@ -8,6 +10,11 @@ ExcelDemo_M::ExcelDemo_M(QWidget *parent)
     ui->setupUi(this);
 
     InitExcelWidget();
+}
+
+ExcelDemo_M::~ExcelDemo_M()
+{
+    delete ui;
 }
 
 void ExcelDemo_M::InitExcelWidget()
@@ -31,10 +38,119 @@ void ExcelDemo_M::InitExcelWidget()
     m_mainWidgetVBoxLayout->setStretch(2, 1);
 
     this->centralWidget()->setLayout(m_mainWidgetVBoxLayout);
+
+    // 菜单设置
+    QMenu *fileMenu = menuBar()->addMenu("文件");
+    m_loadExcelFile = new QAction("加载Excel文件", this);
+    fileMenu->addAction(m_loadExcelFile);
+    connect(m_loadExcelFile, &QAction::triggered, this, &ExcelDemo_M::LoadExcelFile);
+
+    m_saveTableViewData = new QAction("保存数据", this);
+    fileMenu->addAction(m_saveTableViewData);
 }
 
-ExcelDemo_M::~ExcelDemo_M()
+
+// 加载Excel文件
+void ExcelDemo_M::LoadExcelFile()
 {
-    delete ui;
+    m_excelFilePath = QFileDialog::getOpenFileName(this, "打开Excel文件", "", "Excel文件 (*.xlsx *xls)");
+    if (!m_excelFilePath.isEmpty()) {
+        Load();
+    }
 }
 
+void ExcelDemo_M::Load()
+{
+    QAxObject *excelApp = new QAxObject("Excel.Application", this);
+    if (excelApp == nullptr) {
+        qDebug() << "Excel.Application error";
+        return;
+    }
+    QAxObject *workBooks = excelApp->querySubObject("Workbooks");
+    if (workBooks == nullptr) {
+        qDebug() << "Workbooks error";
+        return;
+    }
+    QAxObject *workBook = workBooks->querySubObject("Open(const QString)", m_excelFilePath);
+    if (workBook == nullptr) {
+        qDebug() << "Open(const QString) error";
+        return;
+    }
+
+    // 获取工作表数量
+    QAxObject *workSheets = workBook->querySubObject("Worksheets");
+    if (workSheets == nullptr) {
+        qDebug() << "Worksheets error";
+        return;
+    }
+    int sheetCount = workSheets->property("Count").toInt();
+
+    // 初始化资源
+    InitResource();
+
+    // 遍历表格数据
+    for (int sheetIndex = 0; sheetIndex < sheetCount; ++sheetIndex) {
+
+        // 获取工作表的行数和列数
+        QAxObject *workSheet = workSheets->querySubObject("Item(int)", sheetIndex + 1); // Excel表格从1开始
+        QAxObject *usedRange = workSheet->querySubObject("UsedRange");
+        QAxObject *rows = usedRange->querySubObject("Rows");
+        QAxObject *columns = usedRange->querySubObject("Columns");
+        int rowCount = rows->property("Count").toInt();
+        int columnCount = columns->property("Count").toInt();
+
+        // 获取表名称
+        QString sheetName = workSheet->property("Name").toString();
+
+        // 将单元格数据显示在TableView上
+        QStringList columnHeadstringLabelList; // 存储标题
+        QTableView *tableView = new QTableView(); // 表格视图
+        m_tableViewList.append(tableView);
+        QStandardItemModel *tableModel = new QStandardItemModel(); // 表格数据模型
+        tableModel->setRowCount(rowCount - 1); // 设置显示行数 标题显示在TableView，不占用表格行数
+        tableModel->setColumnCount(columnCount); // 设置显示列数
+        m_excelDataModelList.append(tableModel);
+        for (int row = 0; row < rowCount; ++row) {
+            for (int column = 0; column < columnCount; ++column) {
+                QAxObject *cell = workSheet->querySubObject("Cells(inty, int)", row + 1, column + 1); // Excel文件行列数从1开始
+                QVariant value = cell->property("Value"); //  获取单元格的值
+                if (row == 0) {
+                    columnHeadstringLabelList.append(value.toString()); // 存储标题
+                } else {
+                    QModelIndex index = tableModel->index(row - 1, column);
+                    tableModel->setData(index, value);
+                }
+            }
+        }
+
+        // 设置TableView水平标题
+        tableModel->setHorizontalHeaderLabels(columnHeadstringLabelList);
+        m_sheetHeadStringTableList.append(columnHeadstringLabelList);
+
+        tableView->setModel(tableModel);
+        m_middleTabWidgetInMainWidget->addTab(tableView, sheetName);
+    }
+
+    // 关闭Excel
+    workBook->dynamicCall("Close()");
+    excelApp->dynamicCall("Quit()");
+}
+
+void ExcelDemo_M::InitResource()
+{
+    if (!m_tableViewList.empty()) {
+        for (auto itor = m_tableViewList.begin(); itor != m_tableViewList.end(); ++itor) {
+            delete *itor;
+        }
+    }
+    m_tableViewList.clear();
+
+    if (!m_excelDataModelList.isEmpty()) {
+        for (auto itor = m_excelDataModelList.begin(); itor != m_excelDataModelList.end(); ++itor) {
+            delete *itor;
+        }
+    }
+    m_excelDataModelList.clear();
+
+    m_sheetHeadStringTableList.clear();
+}
--- ExcelDemo_M/exceldemo_m.h
+++ ExcelDemo_M/exceldemo_m.h
@@ -5,6 +5,11 @@
 
 #include <QVBoxLayout>
 #include <QTabWidget>
+#include <QAction>
+#include <QFileDialog>
+#include <QAxObject>
+#include <QTableView>
+#include <QStandardItemModel>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class ExcelDemo_M; }
@@ -23,12 +28,29 @@ private:
 
 private:
     void InitExcelWidget();
+    void Load();
+    void InitResource();
 
 private:
+
+    // Ui界面布局
     QVBoxLayout *m_mainWidgetVBoxLayout;
 
     QWidget *m_topWidgetInMainWidget;
     QTabWidget *m_middleTabWidgetInMainWidget;
     QWidget *m_lowWidgetInMainWidget;
+
+    // 菜单栏
+    QAction *m_loadExcelFile;
+    QAction *m_saveTableViewData;
+
+    // 表格文件数据
+    QString m_excelFilePath;
+    QList<QTableView*> m_tableViewList;
+    QList<QStandardItemModel*> m_excelDataModelList;
+    QList<QStringList> m_sheetHeadStringTableList;
+
+public slots:
+    void LoadExcelFile();
 };
 #endif // EXCELDEMO_M_H
